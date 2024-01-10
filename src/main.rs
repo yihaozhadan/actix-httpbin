5f97ce423de85ad6a7621c7e2b7fd70b7bb6fa61
Hui Zhou <hui.zhou@yum.com>
2024-01-09 18:21:07 -0600

    Implement HTTP info in response
    It is able to return request headers, origin and url info.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,38 @@
-use actix_web::{http::StatusCode, web, middleware, App, HttpResponse, HttpServer, Responder};
+use actix_web::{http::{StatusCode, header::HeaderMap}, web, middleware, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use serde::Serialize;
+use std::collections::BTreeMap;
+
+#[derive(Serialize)]
+struct HttpInfo {
+    headers: BTreeMap<String, String>,
+    origin: String,
+    url: String,
+}
+
+fn convert(headers: &HeaderMap) -> BTreeMap<String, String> {
+    let mut header_hashmap = BTreeMap::new();
+    for (k, v) in headers {
+        let k = k.as_str().to_owned();
+        let v = String::from_utf8_lossy(v.as_bytes()).into_owned();
+        header_hashmap.entry(k).or_insert_with(String::new).push_str(&v)
+    }
+    header_hashmap
+}
+
+async fn index(req : HttpRequest) -> impl Responder {
+    let conn = req.connection_info();
+    let headers = req.headers();
+    let host = conn.host();
+    let path = req.path();
+    let query = req.query_string();
+    let addr = conn.peer_addr();
+    let info = HttpInfo { 
+        headers: convert(headers),
+        origin: addr.unwrap().to_string(),
+        url: if query.is_empty() { format!("http://{}{}", host, path) } else { format!("http://{}{}?{}", host, path, query) },
+    };
+    HttpResponse::Ok().json(info)
+}
 
 async fn status_codes(path: web::Path<u16>) -> impl Responder {
     let status = StatusCode::from_u16(path.into_inner());
@@ -13,7 +47,13 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
-            .service(// prefixes all resources and routes attached to it...
+        .wrap(middleware::Logger::default())
+            .route("/get", web::get().to(index))
+            .route("/post", web::post().to(index))
+            .route("/put", web::put().to(index))
+            .route("/patch", web::patch().to(index))
+            .route("/delete", web::delete().to(index))
+            .service(
                 web::scope("/status")
                     .route("/{status_code}", web::get().to(status_codes))
                     .route("/{status_code}", web::post().to(status_codes))
@@ -21,7 +61,6 @@ async fn main() -> std::io::Result<()> {
                     .route("/{status_code}", web::patch().to(status_codes))
                     .route("/{status_code}", web::delete().to(status_codes))
                 )
-            .wrap(middleware::Logger::default())
     })
     .bind(("0.0.0.0", 8080))?
     .run()
